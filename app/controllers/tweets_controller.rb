ca2b42061b3fde6f4b95fc598a1f177ec29604b0
root <root@DESKTOP-3IBCSAR.localdomain>
2020-02-03 14:23:54 +0000

    i have fixed updating a tweet error

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,55 +1,55 @@
 class TweetsController < ApplicationController
-    before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
 
-    def index
-        @tweets = Tweet.all
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+  end
+
+  def new
+    @tweet = Tweet.new
+  end
+
+  def edit
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+        redirect_to tweets_path, notice: "You have created new tweet!"
+    else
+        redirect_to new_tweet_path
     end
+  end
 
-    def show
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to @tweet,
+      notice: 'Tweet was successfully updated.'
+    else
+      render :edit
     end
+  end
 
-    def new
-        @tweet = Tweet.new
-    end
+  def destroy
+    @tweet.destroy
+    redirect_to tweets_path,
+    notice: 'Tweet was successfully destroyed.'
+  end
 
-    def edit
-    end
-
-    def create
-        @tweet = Tweet.new(tweet_params)
-        if @tweet.save
-            redirect_to tweets_path, notice: "You have created new tweet!"
-        else
-            redirect_to new_tweet_path
-        end
-    end
-
-    def update
-        if @tweet.update(tweet_params)
-            redirect_to @tweet,
-            notice: 'Tweet was successfully updated.'
-        else
-            render :edit
-        end
-      end
+  def confirm
+    @tweet = Tweet.new(tweet_params)
+    render :new if @tweet.invalid?
+  end
     
-      def destroy
-        @tweet.destroy
-          redirect_to tweets_path,
-          notice: 'Tweet was successfully destroyed.'
-      end
-
-      def confirm
-        @tweet = Tweet.new(tweet_params)
-        render :new if @tweet.invalid?
+  private
+    def set_tweet
+      @tweet = Tweet.find(params[:id])
     end
-    
-      private
-        def set_tweet
-          @tweet = Tweet.find(params[:id])
-        end
-    
-        def tweet_params
-          params.require(:tweet).permit(:content)
-        end
+
+    def tweet_params
+      params.require(:tweet).permit(:content)
+    end
 end
